632279b3e52e6b6c8823e49c0499e961d346914c
Al Pacheco <pacheco@fas.harvard.edu>
2015-05-20 11:34:55 -0400

    updated bucket

--- scripts/copysettings.sh
+++ scripts/copysettings.sh
@@ -5,7 +5,7 @@
 # name of your environment's bucket. Replace the value below with the
 # name of the bucket from your environment.
 #
-BUCKETNAME=devops-bootcamp-bucket-1j43zlwyj0sgw
+BUCKETNAME=devops-bootcamp-bucket-1xnqn2mreqrjp
 
 aws s3 cp s3://$BUCKETNAME/settings.php /var/www/html/sites/default/settings.php
 chown apache:apache /var/www/html/sites/default/settings.php
